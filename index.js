42b93744f0ea3573a9f4bff78b2e62a2e038e95d
tdat <datdaty8@gmail.com>
2025-04-02 23:38:26 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,20 @@ const crypto = require('crypto');
 const app = express();
 const port = process.env.PORT || 3002;
 
-const serviceAccount = require('../gjchain/firebase-adminsdk.json');
+// Khởi tạo Firebase với biến môi trường
+let serviceAccount;
+if (process.env.FIREBASE_ADMINSDK) {
+    try {
+        serviceAccount = JSON.parse(process.env.FIREBASE_ADMINSDK);
+    } catch (error) {
+        console.error('Error parsing FIREBASE_ADMINSDK:', error);
+        throw new Error('Invalid FIREBASE_ADMINSDK environment variable');
+    }
+} else {
+    console.log('FIREBASE_ADMINSDK not found, falling back to local file');
+    serviceAccount = require('../gjchain/firebase-adminsdk.json');
+}
+
 admin.initializeApp({ credential: admin.credential.cert(serviceAccount) });
 const walletsCollection = admin.firestore().collection('wallets');
 
